fd370176a94604b9b19944a0bd1f377c86d087ee
ventisk1ze <gkpinchuk@hotmail.com>
2020-07-28 12:23:22 +1000

    Initial commit

---
+++ 1_8.rkt
@@ -0,0 +1,13 @@
+#lang sicp
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x) x)))
+(define (improve guess x)
+  (/ (+ (/ x (* guess guess)) (* 2 guess)) 3))
+(define (good-enough? guess x)
+  (< (abs (- guess (improve guess x))) 0.0001))
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(sqrt 8)
